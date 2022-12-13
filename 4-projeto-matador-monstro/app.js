00aab09d7ea21b5b59be225cd61f347dc5f66034
waicampos <dyego.campos@ifsc.edu.br>
2022-12-13 17:13:00 -0300

    Aula 69 - Implementando Atacar

--- 4-projeto-matador-monstro/app.js
+++ 4-projeto-matador-monstro/app.js
@@ -16,9 +16,28 @@ new Vue({
             this.running = true,
             this.playerLife = 100,
             this.monsterLife = 100
+        },
+
+        attack(special) {
+            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, true)
+        },
+
+        hurt(attribute, min, max, special) {
+            const plus = special ? 5 : 0
+            const hurt = this.getRandon(min + plus, max + plus)
+            this[attribute] = Math.max(this[attribute] - hurt, 0)
+        },
+
+        getRandon(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
         }
     },
     watch: {
-
+        hasResult(value) {
+            if(value)  
+                this.running = false
+        }
     }
 });

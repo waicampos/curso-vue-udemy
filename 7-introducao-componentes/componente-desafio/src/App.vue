9e746ecfb8a78e4e6cadaccc79f447e1bf2f0889
waicampos <dyego.campos@ifsc.edu.br>
2022-12-16 09:34:01 -0300

    Aula 113 - Hora de Praticar: Organizar Projeto em Componentes

---
+++ 7-introducao-componentes/componente-desafio/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <app-menu />
+
+    <app-toolbar />
+
+    <v-content>
+      <app-carousel />
+      <app-table />
+
+    </v-content>
+
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Carousel from './components/Carousel.vue'
+import Footer from './components/Footer.vue'
+import Toolbar from './components/Toolbar.vue'
+import Menu from './components/Menu.vue'
+import Table from './components/table.vue'
+
+export default {
+  components: {
+    'app-carousel': Carousel,
+    'app-footer': Footer,
+    'app-toolbar': Toolbar,
+    'app-menu': Menu,
+    'app-table': Table
+  }
+}
+</script>

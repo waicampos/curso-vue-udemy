10cf1ce12aaa985e5fa74f40cfca44fec4b2ea20
waicampos <dyego.campos@ifsc.edu.br>
2022-12-16 08:47:20 -0300

    Aula 110 - Criar um componente

---
+++ 7-introducao-componentes/componente-intro/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id='app'>
+    <h1>Contadores</h1>
+    <app-contador /> 
+    <app-contador /> 
+  </div>
+</template>
+
+<script>
+  import Contador from './components/Contador.vue'
+
+  export default {
+   name: 'App',
+   components: {
+    "app-contador": Contador
+   }
+}
+</script>
+
+<style>
+  
+</style>

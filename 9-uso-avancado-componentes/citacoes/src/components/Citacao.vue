28cfe16b7ec7e61caa5e499d3cb17ae5f21a70c0
waicampos <dyego.campos@ifsc.edu.br>
2022-12-19 10:00:34 -0300

    Aula 142 - Passando conteúdo com Slots

---
+++ 9-uso-avancado-componentes/citacoes/src/components/Citacao.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        texto: String
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>

f7630e123b0708f9c37dff0bba2c917035d47fe0
waicampos <dyego.campos@ifsc.edu.br>
2022-12-19 14:07:35 -0300

    Aula 158 - Componente Tarefa #01

---
+++ 9-uso-avancado-componentes/projeto-02-tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="task-grid">
+        <template>
+            <Task v-for="task in tasks" :key="task.name" :task="task">
+                {{task.name}}
+            </Task>
+        </template>
+    </div>
+</template>
+
+<script>
+import Task from "./Task.vue"
+export default {
+    name: 'TaskGrid',
+    components: {Task},
+    props: {
+        tasks: {type: Array, required: true}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

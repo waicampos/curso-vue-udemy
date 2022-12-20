7e67e33d224dcb8083823155d328f9eadfe8ae6f
waicampos <dyego.campos@ifsc.edu.br>
2022-12-20 12:03:51 -0300

    Aula - 257 - Navegação Via código (Navegação Imperativa

--- 16-rotas-comunicacao-vue/rotas-exercicios/src/components/users/Usuario.vue
+++ 16-rotas-comunicacao-vue/rotas-exercicios/src/components/users/Usuario.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="usuario">
         <h2>Usuário</h2>
+        <hr>
+        <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        irParaInicio() {
+            //this.$router.push('/')
+            this.$router.push({path: '/'})
+        }
+    }
 }
 </script>
 

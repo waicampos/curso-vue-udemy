dee7a774baead3d9e37ce2c1eab72d0f75f95f15
waicampos <dyego.campos@ifsc.edu.br>
2022-12-20 15:25:17 -0300

    Aula 263 - Navegando para Rotas ANinhadas

--- 16-rotas-comunicacao-vue/rotas-exercicios/src/components/users/UsuarioEditar.vue
+++ 16-rotas-comunicacao-vue/rotas-exercicios/src/components/users/UsuarioEditar.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="usuario-editar">
         <h3>Usuário Editar</h3>
+        <hr>
+        <p><strong>Código:</strong>{{id}}</p>
     </div>
 </template>
 
 <script>
 export default {
-
+    props: ['id']
 }
 </script>
 

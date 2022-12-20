1c6e82bfc3f8ed5354453a796fe44abdfe81cade
waicampos <dyego.campos@ifsc.edu.br>
2022-12-20 15:51:17 -0300

    Aula 264 - Tornando Router links mais dinâmicos

--- 16-rotas-comunicacao-vue/rotas-exercicios/src/components/users/UsuarioDetalhe.vue
+++ 16-rotas-comunicacao-vue/rotas-exercicios/src/components/users/UsuarioDetalhe.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="usuario-detalhe">
     <h3>Usuário Detalhe</h3>
-    <p><strong>Código:</strong>{{id}}</p>
+    <p><strong>Código:</strong>{{id}} / {{$route.params.id}}</p>
+  
+  <router-link tag="button" primario
+    :to="`/usuario/${$route.params.id}/editar`">
+      Editar
+  </router-link>
+    
+  <!--<router-link tag="button" primario
+    to="`/usuario/${id}/editar`">
+      Editar
+  </router-link>-->
+    
   </div>
 </template>
 

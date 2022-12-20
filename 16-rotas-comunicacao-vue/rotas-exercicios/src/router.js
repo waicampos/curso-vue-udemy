58ca2694d68c3efa7c1ff489a82d9a21180ccb09
waicampos <dyego.campos@ifsc.edu.br>
2022-12-20 16:42:28 -0300

    Aula 272 - Controlando o comportamento de Rolagem (Scroll)

--- 16-rotas-comunicacao-vue/rotas-exercicios/src/router.js
+++ 16-rotas-comunicacao-vue/rotas-exercicios/src/router.js
@@ -12,6 +12,15 @@ Vue.use(Router)
 
 export default new Router({
     mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+        if(savedPosition){
+            return savedPosition
+        } else if(to.hash) {
+            return {selector: to.hash}
+        } else {
+            return{x:0, y:1000}
+        }
+    },
     routes: [{
         path: '/',
         name: 'inicio',
